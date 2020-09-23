b50104b9fecfddc5f957c5bf2fcb1b455dcbec9e
sameer sri vishnu <sameersrivishnu@gmail.com>
2020-09-23 15:38:57 +0530

    Final

---
+++ db/migrate/20200923075904_create_questions.rb
@@ -0,0 +1,11 @@
+class CreateQuestions < ActiveRecord::Migration[6.0]
+  def change
+    create_table :questions do |t|
+      t.string :question   
+      t.integer :topic_id
+      t.integer :user_id    	
+      t.timestamps
+    end
+  end
+end
+
